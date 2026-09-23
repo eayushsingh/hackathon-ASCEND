da410a5dddf6df214f52295089448af8ca50eb24
Ayush Singh <ayushhsinghhe01@gmail.com>
2026-09-23 21:25:11 +0530

    Fix syntax error in middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    cont a = new 
+    
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
